4105a8d73d2da8b9f7c7c341e8720e8b92c21730
Tinka8 <lenmatka818@gmail.com>
2023-12-21 18:22:33 +0100

    style: add custom color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,7 +10,8 @@ module.exports = {
         "prussian": "#17293a",
         "midnight": "#004346",
         "mint": "#07bc8a",
-        "verdigris": "#5bc0be"
+        "verdigris": "#5bc0be",
+        "gray": "#f1f6f9"
       },
     },
   },
